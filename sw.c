3d1275966155c086e8f339d8d2d42ed92cb994bb
AnaRitaFerreira <ei12052@fe.up.pt>
2015-04-13 22:13:49 +0100

    sw
    procura as palavras de words no ficheiro, falta corrigir o output

--- sw.c
+++ sw.c
@@ -2,27 +2,62 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+ #include <stdlib.h>
 
 #define BUFFER_SIZE 512
 
 int main(int argc, char *argv[]){
 	int fd=0;
+	FILE * f1;
+	//FILE * fd;
+	char word[100];
+	char c;
+	//char  line[100];
+   // size_t len = 0;
+    //ssize_t rd;
+    pid_t pid;
+
 	//unsigned char buf[BUFFER_SIZE]=0;
-	
-	fd=open("test.txt", O_WRONLY | O_CREAT | O_EXCL, 0644);
+	//fd=fopen("test.txt","w");
+	fd=open("test.txt", O_WRONLY | O_CREAT, 0644);
 	if(fd==-1){
 		perror("Erro");
 		close(fd);
 		return 1;
 	}
-	fd2=open("words.txt",O_RDONLY);
-	if(fd2==-1){
+	//fd2=open("words.txt",O_RDONLY);
+	/*if(fd2==-1){
 		perror("Erro");
 		close(fd2);
 		return 2;
-	}
-	while(){
-	execlp("grep","grep","-o -n", word, "test.txt", NULL);
-	}
+	}*/
+	f1=fopen("words.txt","r");
+	if (f1 == NULL){
+          perror("Erro");
+          fclose(f1);
+          return 2;
+         
+	   }
+	dup2(fd,STDOUT_FILENO);
+	/*while ((rd = getline(&line, &len, f1)) != -1) {
+		if((pid=fork())==-1){
+			perror("fork error");
+		}
+		else if(pid==0){
+		execlp("grep","grep","-o", "-n", line, "1.txt", NULL);
+		}
+       }*/
+	do {
+      c = fscanf(f1,"%s",word);									//got one word from the file 
+      if((pid=fork())==-1){
+			perror("fork error");
+		}
+		else if(pid==0){
+		execlp("grep","grep","-o", "-n", word, "1.txt", NULL);
+		}
+   } while (c != EOF);           								  //repeat until EOF 
+	
+	fclose(f1);
+	 
 	return 0;
 }
