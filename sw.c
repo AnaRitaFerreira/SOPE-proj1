602ef0010dbd8da00cd4067ce910d2ee57c4fb50
AnaRitaFerreira <ei12052@fe.up.pt>
2015-04-17 18:41:05 +0100

    sw finished

--- sw.c
+++ sw.c
@@ -8,45 +8,46 @@
 #define BUFFER_SIZE 512
 
 int reverseOutput(){
+	sleep(1);
 	int pos=0, i ,index,size=0;
 	char a;
 	FILE * t;
 	FILE * temp;
 	
-	temp=fopen("temp.txt","w");
+	temp=fopen("temp.txt","w"); // escreve
 	if (temp == NULL){
           perror("Erro");
           fclose(temp);
           return 3;
 	}
-	
-	t=fopen("test.txt","r");
+	t=fopen("test.txt","r"); // lê
 	if (t == NULL){
           perror("Erro");
           fclose(t);
           return 4;
 	}
-
 	do{ 
-		char buf[100];
-		char noLine[50];
-		char word[50];
-		a=fscanf(t,"%s",buf);
+		char buf[BUFFER_SIZE]="";
+		char noLine[BUFFER_SIZE]="";
+		char word[BUFFER_SIZE]="";
+		char str[BUFFER_SIZE]="";
+		a=fscanf(t,"%s",buf); // lê linha 
 		size=strlen(buf);
 		for(i=0; i<size;i++){
 			 if(buf[i] != ':') 
-			 noLine[i] = buf[i] ;
-			 else pos = i+1; 
-			 break; 
+				noLine[i] = buf[i] ;
+			 else {
+				pos = i+1; 
+				break; 
 			 }
-			 
-		for(i=pos, index=0;i<size;i++, index++) {
+		}		 
+		for(i=pos, index=0; i<size; i++, index++) {
 			 word[index] = buf[i]; 
-			 }
-		strcat(word," : 1-");
-		strcat(word,noLine);
-		strcat(word,"\n");
-		fputs(word,temp); 
+		}
+		sprintf(str, "%s : 1-%s\n", word, noLine);
+		int strSize=strlen(word);
+		if(strSize!=0)
+			fputs(str,temp);
 	}while(a!=EOF);
 	fclose(t);
 	fclose(temp);
@@ -54,35 +55,25 @@ int reverseOutput(){
 }
 
 int main(int argc, char *argv[]){
-	int fd=0;
+	int f2=0;
 	FILE * f1;
-	char word[100];
+	char word[BUFFER_SIZE];
 	char c;
-	
     pid_t pid;
-
-	//unsigned char buf[BUFFER_SIZE]=0;
 	
-	fd=open("test.txt", O_WRONLY | O_CREAT, 0644);
-	if(fd==-1){
-		perror("Erro");
-		close(fd);
-		return 1;
-	}
-	//fd2=open("words.txt",O_RDONLY);
-	/*if(fd2==-1){
-		perror("Erro");
-		close(fd2);
-		return 2;
-	}*/
 	f1=fopen("words.txt","r");
 	if (f1 == NULL){
           perror("Erro");
           fclose(f1);
           return 2;
 	   }
-	dup2(fd,STDOUT_FILENO);
-	
+	f2=open("test.txt", O_WRONLY | O_CREAT, 0644);
+	if(f2==-1){
+		perror("Erro");
+		close(f2);
+		return 1;
+	}
+	dup2(f2,STDOUT_FILENO);
 	do {
       c = fscanf(f1,"%s",word);									//got one word from the file 
       if((pid=fork())==-1){
@@ -92,9 +83,8 @@ int main(int argc, char *argv[]){
 		execlp("grep","grep","-o", "-n", word, "1.txt", NULL);
 		}
    } while (c != EOF);          								  //repeat until EOF 
-	
 	fclose(f1);
-	
+	close(f2);
 	reverseOutput();
 	return 0;
 }
